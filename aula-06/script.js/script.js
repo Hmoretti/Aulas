24dc3560ce5b7bb49c35ed325ac822728fe55854
Hmoretti <moretti.hernani@gmail.com>
2019-11-05 21:59:02 -0300

    aprendendo javascript

---
+++ aula-06/script.js/script.js
@@ -0,0 +1,14 @@
+let login = "abelha";
+let senha = "ovoazul";
+
+let putUsuario = prompt("insira seu usuario")
+let putsenha = prompt(" insira sua senha")
+
+if (login === putUsuario && senha === putsenha){
+    alert("logado com sucesso")
+}
+else{
+    alert("errado")
+          
+}
+
