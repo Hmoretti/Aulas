29cc4b96685832541db42b771588bcab41a15669
JS-jopepe <jovenel.s@outlook.com>
2019-11-06 21:06:36 -0300

    aprendendo for e while

---
+++ aula-07/js/script.js
@@ -0,0 +1,13 @@
+let animes = [ "naruto", "naruto1", "naruto2" , "naturo3"];
+// let i = 0;
+
+// while( i< animes.length){
+    // console.log(animes[i]);
+    // i++;
+// }
+
+// FOR
+
+for (let e = 0; e < animes.length; e++){
+    console.log(animes[e]);
+}
