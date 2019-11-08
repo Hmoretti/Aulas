ee339ff964f0f38dda989737bb9030c20a9c9742
Hmoretti <moretti.hernani@gmail.com>
2019-11-07 21:55:58 -0300

    mudando botão e acoes

---
+++ aula-08/js/script.js
@@ -0,0 +1,12 @@
+let botao = document.querySelector('button');
+
+function mudarBotao(){
+    botao.style.backgroundColor = "blue";
+}
+
+botao.onclick = mudarBotao;
+
+let botao = document.querySelector('button')
+
+
+// usar tuggle
