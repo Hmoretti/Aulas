fa8287821dbc0c553c5953c94c2a6e78d5a082f7
Hmoretti <moretti.hernani@gmail.com>
2019-11-13 21:58:42 -0300

    exercicios em python

---
+++ aula-11/exer.py
@@ -0,0 +1,28 @@
+# n1 = int(input('digite numero 1'))
+# n2 = int(input('digite numero 2'))
+# n3 = int(input('digite numero 3'))
+# n4 = int(input('digite numero 4'))
+
+# media = (n1 + n2 + n3 + n4)/4
+
+# print(media)
+
+###############################
+
+# palavra = input("Digite sua palavra ou frase")
+# letra = input("Digite sua letra")
+
+# quantidade = palavra.count(letra)
+
+# print(quantidade)
+
+
+###############################
+
+num1 = int(input("Digite seu numero"))
+num2 = int(input("Digite seu numero"))
+qual =  input("Qual voce quer? Soma, Subtração, Divisão ou Multiplicação")
+
+def conta():
+    if qual( num1+num2  )
+    print("")
